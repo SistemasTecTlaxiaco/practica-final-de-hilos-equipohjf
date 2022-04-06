ab79f705f7a41f03494ed08eee118a2cc1ce7e34
HEIDI LUCY BAUTISTA SANJUAN <heidi_lbs@tlaxiaco.tecnm.mx>
2022-04-06 14:44:04 -0500

    BarraProgreso

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -38,22 +39,31 @@
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tiempo = new System.Windows.Forms.Label();
+            this.progreso = new System.Windows.Forms.ProgressBar();
+            this.btnTiempo = new System.Windows.Forms.Button();
+            this.temporizador = new System.Windows.Forms.Timer(this.components);
+            this.Carga = new System.Windows.Forms.Label();
+            this.numCarga = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCarga)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(117, 12);
+            this.textBox1.Location = new System.Drawing.Point(88, 10);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(174, 22);
+            this.textBox1.Size = new System.Drawing.Size(132, 20);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(297, 12);
+            this.button1.Location = new System.Drawing.Point(223, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -63,17 +73,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 38);
+            this.label1.Size = new System.Drawing.Size(76, 30);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(538, 12);
+            this.button2.Location = new System.Drawing.Point(404, 10);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(56, 19);
             this.button2.TabIndex = 3;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
@@ -82,9 +94,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(166, 161);
+            this.pictureBox1.Location = new System.Drawing.Point(124, 131);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(115, 99);
+            this.pictureBox1.Size = new System.Drawing.Size(86, 80);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
@@ -92,18 +105,20 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(19, 122);
+            this.checkBox1.Location = new System.Drawing.Point(14, 99);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(141, 20);
+            this.checkBox1.Size = new System.Drawing.Size(113, 17);
             this.checkBox1.TabIndex = 5;
             this.checkBox1.Text = "Girar a la izquierda";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(166, 119);
+            this.button3.Location = new System.Drawing.Point(124, 97);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(56, 19);
             this.button3.TabIndex = 6;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
@@ -111,17 +126,93 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(261, 119);
+            this.textBox2.Location = new System.Drawing.Point(196, 97);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
             this.textBox2.TabIndex = 7;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
+            // tiempo
+            // 
+            this.tiempo.AutoSize = true;
+            this.tiempo.Location = new System.Drawing.Point(220, 267);
+            this.tiempo.Name = "tiempo";
+            this.tiempo.Size = new System.Drawing.Size(49, 13);
+            this.tiempo.TabIndex = 8;
+            this.tiempo.Text = "00:00:00";
+            this.tiempo.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // progreso
+            // 
+            this.progreso.Location = new System.Drawing.Point(146, 296);
+            this.progreso.Name = "progreso";
+            this.progreso.Size = new System.Drawing.Size(193, 23);
+            this.progreso.Step = 1;
+            this.progreso.TabIndex = 9;
+            // 
+            // btnTiempo
+            // 
+            this.btnTiempo.Location = new System.Drawing.Point(204, 331);
+            this.btnTiempo.Name = "btnTiempo";
+            this.btnTiempo.Size = new System.Drawing.Size(75, 23);
+            this.btnTiempo.TabIndex = 10;
+            this.btnTiempo.Text = "Cargar";
+            this.btnTiempo.UseVisualStyleBackColor = true;
+            this.btnTiempo.Click += new System.EventHandler(this.btnTiempo_Click);
+            // 
+            // temporizador
+            // 
+            this.temporizador.Enabled = true;
+            this.temporizador.Interval = 1000;
+            this.temporizador.Tick += new System.EventHandler(this.temporizador_Tick);
+            // 
+            // Carga
+            // 
+            this.Carga.AutoSize = true;
+            this.Carga.Location = new System.Drawing.Point(345, 306);
+            this.Carga.Name = "Carga";
+            this.Carga.Size = new System.Drawing.Size(35, 13);
+            this.Carga.TabIndex = 11;
+            this.Carga.Text = "Carga";
+            // 
+            // numCarga
+            // 
+            this.numCarga.Increment = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numCarga.Location = new System.Drawing.Point(386, 306);
+            this.numCarga.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numCarga.Minimum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numCarga.Name = "numCarga";
+            this.numCarga.Size = new System.Drawing.Size(74, 20);
+            this.numCarga.TabIndex = 12;
+            this.numCarga.Value = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(637, 450);
+            this.ClientSize = new System.Drawing.Size(478, 366);
+            this.Controls.Add(this.numCarga);
+            this.Controls.Add(this.Carga);
+            this.Controls.Add(this.btnTiempo);
+            this.Controls.Add(this.progreso);
+            this.Controls.Add(this.tiempo);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.checkBox1);
@@ -130,9 +221,12 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCarga)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,6 +243,12 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label tiempo;
+        private System.Windows.Forms.ProgressBar progreso;
+        private System.Windows.Forms.Button btnTiempo;
+        private System.Windows.Forms.Timer temporizador;
+        private System.Windows.Forms.Label Carga;
+        private System.Windows.Forms.NumericUpDown numCarga;
     }
 }
 
